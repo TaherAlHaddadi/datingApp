bc67fa18442cdc3bb6a632d90a554e5116ed8d67
TaherAlHaddadi <hauntedhill32@gmail.com>
2023-03-21 13:52:31 +0400

    updating

--- API/Program.cs
+++ API/Program.cs
@@ -1,14 +1,4 @@
-using System.Globalization;
-using System.Text;
-using API.Data;
 using API.Extensions;
-using API.Interfaces;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
-
 var builder = WebApplication.CreateBuilder(args);//Create a web application instance
 
 // Add services to the container.
@@ -16,12 +6,12 @@ var builder = WebApplication.CreateBuilder(args);//Create a web application inst
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
-
+builder.Services.AddCors();
 
 var app = builder.Build();// services container
 
 // Configure the HTTP request pipeline.
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.UseAuthentication(); // do you have a valid token?
 app.UseAuthorization(); // OK! you have token what things are you allowed to do ?
